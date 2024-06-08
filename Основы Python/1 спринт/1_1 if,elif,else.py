f010d6718ec730b232624ea732fcb9a2d106725c
Anastasiia708 <anikinaanas@mail.ru>
2024-06-08 13:49:20 +0300

    Срезы в строках

---
+++ Основы Python/1 спринт/1_1 if,elif,else.py
@@ -0,0 +1,14 @@
+if False or True:
+# false or x < 45 and x != 42
+# false or true
+    print("Выражение вернуло True!")
+else:
+    print("Выражение вернуло False!")
+"""""
+
+chinese = False
+seafood = False
+italian = True
+
+if not(chinese or seafood) and italian:
+    print('Заказываем итальянскую пасту!')
